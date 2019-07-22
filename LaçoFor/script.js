f702a0db88a419acc75ef33c79a8a376b7b3b417
RogerLeitel8 <roger.leite@etec.sp.gov.br>
2019-07-22 17:59:16 -0300

    Adicionar os laços de repetição

---
+++ LaçoFor/script.js
@@ -0,0 +1,37 @@
+// for(let contador=0; contador<4; contador++){
+//     console.log(contador);
+// }
+
+
+// let listaChamada = ["almeida Tamarindo", "jorge Aragão","jubileu didi","ronaldinho Gaúcho", "akida sorte","minalba granada","miranha da silva", "Cleytinn Rasta"];
+// for (let contador=0; contador <listaChamada.length; contador++){
+//     console.log(listaChamada[contador]);
+// }
+
+
+// let listaChamada = ["almeida Tamarindo", "jorge Aragão","jubileu didi","ronaldinho Gaúcho", "akida sorte","minalba granada","miranha da silva", "Cleytinn Rasta"];
+// for (let capivara in listaChamada){
+//    console.log(listaChamada[capivara]);
+// }
+
+
+// let listaChamada = ["almeida Tamarindo", "jorge Aragão","jubileu didi","ronaldinho Gaúcho", "akida sorte","minalba granada","miranha da silva", "Cleytinn Rasta"];
+// for (let aluno of listaChamada){
+//    console.log(aluno);
+//}
+
+
+// let filmes = [
+// {nome:"Rei Leão", classificação:12, estilo:"animação"},
+// {nome:"Vingadores", classificação:16, estilo:"Ação"},
+// {nome: "KillBill", classificação:25, estilo:"muito sangue"},
+// {nome: "O iluminado", classificação:23, estilo:"perturbador"},
+// {nome: "Tropa de elite", classificação:27, estilo:"sangue e tapa na cara"}
+// ]
+// idade = prompt ("Informe Sua idade")
+
+// for (let filme of filmes){
+//     if(idade >= filme.classificação){
+//         console.log(`Você pode assistir o ${filme.nome}`);
+//     }
+// }
---
+++ LaçoWhile/script.js
@@ -0,0 +1,6 @@
+let nomeCapivara = "Barney";
+let contador = 0;
+while(contador <5){
+    console.log(nomeCapivara);
+    contador ++;
+}
